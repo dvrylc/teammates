0342f05c803026af2d7bd5b6e52f56277862071c
Joshua Wong <joshwonghc@gmail.com>
2020-02-26 18:01:03 +0800

    Remove unused stuff

--- src/main/java/teammates/ui/webapi/action/SearchLinksAction.java
+++ src/main/java/teammates/ui/webapi/action/SearchLinksAction.java
@@ -8,7 +8,6 @@ import java.util.Map;
 import java.util.Set;
 
 import teammates.common.datatransfer.attributes.AccountAttributes;
-import teammates.common.datatransfer.attributes.CourseAttributes;
 import teammates.common.datatransfer.attributes.InstructorAttributes;
 import teammates.common.datatransfer.attributes.StudentAttributes;
 import teammates.common.exception.UnauthorizedAccessException;
