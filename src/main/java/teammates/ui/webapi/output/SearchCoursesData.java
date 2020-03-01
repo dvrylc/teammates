6269850501897facf4b82dd1da0cd25e62a1ab66
Joshua Wong <joshwonghc@gmail.com>
2020-03-01 17:40:22 +0800

    Draft

---
+++ src/main/java/teammates/ui/webapi/output/SearchCoursesData.java
@@ -0,0 +1,22 @@
+package teammates.ui.webapi.output;
+
+import java.util.List;
+
+public class SearchCoursesData extends ApiOutput {
+    private List<SearchCoursesCommonData> students;
+    private List<SearchCoursesCommonData> instructors;
+
+    public SearchCoursesData(List<SearchCoursesCommonData> students,
+            List<SearchCoursesCommonData> instructors) {
+        this.students = students;
+        this.instructors = instructors;
+    }
+
+    public List<SearchCoursesCommonData> getStudents() {
+        return students;
+    }
+
+    public List<SearchCoursesCommonData> getInstructors() {
+        return instructors;
+    }
+}
